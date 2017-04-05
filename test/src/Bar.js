2920d7ffcd25a88eb2c478b5f8cad080efc6d6bc
Caleb at school <caleb.mccann@bellevuecollege.edu>
2017-04-04 17:15:46 -0700

    added test file

---
+++ test/src/Bar.js
@@ -0,0 +1,15 @@
+/**
+ * Created by bcuser on 4/4/17.
+ */
+
+import React from 'react';
+
+class Bar extends React.Component {
+    render() {
+        return (
+            <h1>Bar</h1>
+        )
+    }
+}
+
+export default Bar;
---
+++ test/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import Bar from './Bar';
+import './index.css';
+
+ReactDOM.render(
+<div>
+    <App />
+    <Bar />
+</div>,
+  document.getElementById('root')
+
+);
