d656c01a89b32a28901c09014e66f8951f47b3bd
Caleb at school <caleb.mccann@bellevuecollege.edu>
2017-04-06 16:48:12 -0700

    Added Week01-ReactBasics

---
+++ Week01-ReactBasics/main.js
@@ -0,0 +1,14 @@
+/**
+ * Created by bcuser on 4/6/17.
+ */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+//importing the ReactBasics class/object
+import { ReactBasics } from './ReactBasics.js';
+
+//<ReactBasics/> is the class from ReactBasics.js
+
+//render the class into the root element
+ReactDOM.render(<ReactBasics/>, document.getElementById('root'));
