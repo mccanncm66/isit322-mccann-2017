41a9d9f035a141f4d6d0d2485fd345205ae7f943
Caleb-Home <caleb.mccann@bellevuecollege.edu>
2017-05-01 21:54:32 -0700

    finished git converter

--- GitExplorer/git-convert/git-convert.js
+++ GitExplorer/git-convert/git-convert.js
@@ -1,32 +1,44 @@
 /**
  * Created by bcuser on 4/30/17.
  */
+const unknown = 'unknown';
+const PARAGRAPH = 0;
+const TEXT = 1;
+const DEFAULT = PARAGRAPH;
+const types = ['paragraph', 'text'];
 var debug = require('debug')('git-convert');
 debug('this is a test');
 
-var fs = require('fs');
+var fs = require('mz/fs');
 var json = {};
-
 function readFile(fileName) {
-    return new Promise(function(resolve, reject) {
-        fs.readFile(fileName, '', function(err, data) {
-            if (err) throw err;
-            var obj = JSON.parse(data);
-            resolve(JSON.stringify(obj));
-            //console.log(json);
-        });
+    return new Promise(function (resolve, reject) {
+        fs.readFile('./git-user.json')
+            .then(contents => resolve(contents))
+            .catch(err => debug(err));
     });
 }
 
-readFile('git-user.json').then(function(text) {
-    // CODE OMITTED. This is where you solve the core of the assignment.
-    var parseData = JSON.parse(text);
-    console.log(parseData.login);
-});
-console.log(JSON.stringify(json));
-for (var property in json) {
-    if (json.hasOwnProperty(property)) {
-        // YOUR WORK HERE
+readFile('git-user.json')
+    .then(function (text) {
+        return Promise.resolve(JSON.parse(text));
+    }).then(function (parsedJson) {
+
+    var fieldDefinition = [];
+
+    for (var property in parsedJson) {
+
+        if (parsedJson.hasOwnProperty(property)) {
+            debug(property);
+            debug(parsedJson[property]);
+            fieldDefinition.push({
+                id: property,
+                label: property,
+                type: types[DEFAULT],
+                sample: property + ' ' + unknown
+            });
+        }
     }
-}
-console.log( /* LOG YOUR ARRAY OF OBJECTS */)
+
+    console.log(fieldDefinition);
+});
